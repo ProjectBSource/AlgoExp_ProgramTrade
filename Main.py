6d3882a2f18414b05804e43c7324a1b15d9ad1e7
PatrickFung <patrick.fung@team-c.com>
2023-04-04 15:58:50 +0800

    Message handling

--- Main.py
+++ Main.py
@@ -15,6 +15,20 @@ while(True):
     for message in tgAPIc.getMessage():
         if(message.id is not None and message.text is not None):
             Common.print_and_logging( "[" + str(message.id) + "]" + message.text )
-            tgAPIc.sendMessage(phonenumber, message.text)
+            if("Algoexpsignal report" in message.text):
+                action = None
+                price = (int((message.text[message.text.find(" at ")+4:message.text.find(" on ")]).replace(",","")))
+                qty = 1
+                if("Strategy1_call" in message.text):
+                    action = "BUY"
+                if("Strategy1_Put" in message.text):
+                    action = "SELL"
+                if("Close_call" in message.text):
+                    action = "SELL"
+                if("Close_Put" in message.text):
+                    action = "BUY"
+                message_for_IB_trade = "{} {} MHI@{} ".format(action, str(qty), str(price))
+                
+            tgAPIc.sendMessage(phonenumber, message_for_IB_trade)
     print(str(datetime.now()) + "-------------------------------------------------")
     time.sleep(1)
